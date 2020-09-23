b2cfb9ecf374c3870d2114c47793f81ea223a696
ms-iotkithol-jp <ms-iotkithol-jp@outlook.jp>
2020-09-23 16:37:32 +0900

    Add pressure and multi device showing

---
+++ SampleViewer/scripts/databysignalr-table.js
@@ -0,0 +1,107 @@
+﻿$(document).ready(function() {
+    const apiBaseUrl = "https://egsignalrservicehub20190620.azurewebsites.net/";
+    let data = { ready: false };
+    textArrivedElem = document.getElementById("textArrived");
+    textMessageElem = document.getElementById("textMessage");
+
+    var receiveddata={};
+    var deviceEntries ={};
+
+    var receivedTable = document.getElementById("receiveddevices");
+    var receivedTableNumOfCells = receivedTable.rows[0].cells.length;
+    var receivedTableNumOfRows = receivedTable.rows.length;
+
+    function createDeviceEntry(deviceid){
+        var rows=[];
+        var newCell = document.createElement("table");
+        for(i=0;i<4;i++) {
+            rows.push(newCell.insertRow(-1));
+            var cell = rows[i].insertCell(-1);
+            cell.appendChild(document.createTextNode(""));
+        }
+        rows[0].cells[0].firstChild.data = deviceid;
+        return newCell;
+    }
+
+    function updateDeviceEntry(message){
+        var targetCell=receiveddata[message.deviceid];
+        targetCell.rows[1].cells[0].firstChild.data = message.epoctime;
+        targetCell.rows[2].cells[0].firstChild.data = message.sensor;
+        targetCell.rows[3].cells[0].firstChild.data = message.valuef;
+    }
+
+    getConnectionInfo().then(function(info) {
+        let accessToken = info.accessToken;
+
+        const options = {
+            accessTokenFactory: function() {
+                if (accessToken) {
+                    const _accessToken = accessToken;
+                    accessToken = null;
+                    return _accessToken;
+                } else {
+                    return getConnectionInfo().then(function(info) {
+                        return info.accessToken;
+                    });
+                }
+            }
+        };
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(info.url, options)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        connection.on("SendData", function(obj) {
+            var currenttime = new Date();
+            var arrived = currenttime.getFullYear() + '/' + currenttime.getMonth() + '/' + currenttime.getDay() + '-'+currenttime.getHours()+':'+currenttime.getMinutes()+':'+currenttime.getSeconds();
+            var recieved = JSON.parse(obj);
+            var message = recieved;
+            console.log('message:' + JSON.stringify(recieved));
+            textArrivedElem.innerHTML = arrived;
+            textMessageElem.innerHTML = JSON.stringify(recieved);
+
+            if (!message.epoctime || !message.deviceid || !message.sensor) {
+                return;
+            }
+            if (!receiveddata[message.deviceid]){
+                receiveddata[message.deviceid] = createDeviceEntry(message.deviceid);
+                var numOfDevice = Object.keys(receiveddata).length;
+                if ((Object.keys(receiveddata).length%receivedTableNumOfCells)===0) {
+                    receivedTable.insertRow(-1);
+                    for(i=0;i<receivedTableNumOfCells;i++){
+                        receivedTable.rows[receivedTableNumOfRows].insertCell(-1);
+                    }
+                    receivedTableNumOfRows++;
+                }
+                var indexRow = Math.floor((Object.keys(receiveddata).length-1)/receivedTableNumOfCells);
+                var indexCell = (Object.keys(receiveddata).length-1)%receivedTableNumOfCells;
+                receivedTable.rows[indexRow].cells[indexCell].appendChild(receiveddata[message.deviceid]);
+                deviceEntries[message.deviceid]=receiveddata[message.deviceid];
+            }
+          //  receiveddata[message.deviceid] ={'epoctime':message.epoctime,'sensor-name':message.sensor,'sensor-value':message.valuef};
+            updateDeviceEntry(message);
+        });
+
+
+        connection.onclose(function() {
+            console.log('disconnected');
+            setTimeout(function() { startConnection(connection); }, 2000);
+        });
+
+        console.log('connecting...');
+        connection.start().then(() => data.ready = true)
+            .catch(console.error);
+    });
+
+    function getConnectionInfo() {
+        return $.post({
+            url: `${apiBaseUrl}/api/SignalRInfo`,
+            data: {}
+        }).done(function(resp, textStatus, jqXHR) {
+            return resp.data;
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        });
+    }
+});
--- Test/WpfAppSendTest/MainWindow.xaml.cs
+++ Test/WpfAppSendTest/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Azure.EventHubs;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,51 +15,83 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Azure.EventHubs;
 
-namespace WpfAppSendTest {
+namespace WpfAppSendTest
+{
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window {
-
-        public MainWindow () {
-            InitializeComponent ();
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
             this.Loaded += MainWindow_Loaded;
         }
 
-        private void MainWindow_Loaded (object sender, RoutedEventArgs e) {
-            var connectionStringBuilder = new EventHubsConnectionStringBuilder (ehConnectionString) {
-                EntityPath = ehName
-            };
-            eventHubClient = EventHubClient.CreateFromConnectionString (connectionStringBuilder.ToString ());
-            random = new Random (DateTime.Now.Millisecond);
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbCS.Text = ConfigurationManager.ConnectionStrings["eventhub-connectionstring"].ConnectionString;
+            tbDeviceId.Text = "device1";
+
+            random = new Random(DateTime.Now.Millisecond);
         }
 
-        readonly string ehConnectionString = "< Event Hub Connection String >";
         readonly string ehName = "datasource";
         EventHubClient eventHubClient;
         Random random;
 
         private double lastTemperature = 20;
         private double lastHumidity = 50;
-        private async void ButtonSend_Click (object sender, RoutedEventArgs e) {
-            var data = new {
+        private double lastPressure = 1000;
+        private async void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            var data = new
+            {
+                deviceid = tbDeviceId.Text,
                 temperature = lastTemperature,
                 humidity = lastHumidity,
-                measured_time = DateTime.Now.ToString ("yyyy/MM/ddTHH:mm:ss")
+                pressure = lastPressure,
+                measured_time = DateTime.Now.ToString("yyyy/MM/ddTHH:mm:ss")
             };
 
-            var message = Newtonsoft.Json.JsonConvert.SerializeObject (data);
-            var ehMsg = new EventData (Encoding.UTF8.GetBytes (message));
-            await eventHubClient.SendAsync (ehMsg);
-            lastTemperature += (1 - 2 * random.NextDouble ());
-            lastHumidity += (1 - 2 * random.NextDouble ());
-            if (lastHumidity > 100) {
+            var message = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            var ehMsg = new EventData(Encoding.UTF8.GetBytes(message));
+            await eventHubClient.SendAsync(ehMsg);
+
+            var sb = new StringBuilder(tbLog.Text);
+            var writer = new StringWriter(sb);
+            writer.WriteLine($"Send - {message}");
+            tbLog.Text = sb.ToString();
+
+            lastTemperature += (1 - 2 * random.NextDouble());
+            lastHumidity += (1 - 2 * random.NextDouble());
+            if (lastHumidity > 100)
+            {
                 lastHumidity = 100;
-            } else if (lastHumidity < 0) {
+            }
+            else if (lastHumidity < 0)
+            {
                 lastHumidity = 0;
             }
+            lastPressure += (1 - 2 * random.NextDouble());
+        }
+
+        private void buttonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(tbCS.Text)
+            {
+                EntityPath = ehName
+            };
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+            buttonSend.IsEnabled = true;
+
+        }
+
+        private void buttonClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            tbLog.Text = "";
         }
     }
 }
